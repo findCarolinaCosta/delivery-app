2fbdd471e2395f49afdfb1d27dea79abef57bcc1
Paulo Henrique Lima <limapaulobsb@gmail.com>
2022-06-01 18:04:22 -0300

    Fix sequelize

--- back-end/src/database/models/salesproduct.js
+++ back-end/src/database/models/salesproduct.js
@@ -1,12 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  const SalesProduct = sequelize.define("SalesProduct", {
-    sale_id: DataTypes.INTEGER,
-    product_id: DataTypes.INTEGER,
-    quantity: DataTypes.INTEGER,
-  },{
-    underscored: true,
-    timestamps: false,
-  });
+  const SalesProduct = sequelize.define(
+    "SalesProduct",
+    {
+      sale_id: DataTypes.INTEGER,
+      product_id: DataTypes.INTEGER,
+      quantity: DataTypes.INTEGER,
+    },
+    {
+      tableName: 'SalesProducts',
+      underscored: true,
+      timestamps: false,
+    }
+  );
 
   return SalesProduct;
 };
