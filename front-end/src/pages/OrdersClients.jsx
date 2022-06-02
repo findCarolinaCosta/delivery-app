2e63b00a7befc83b78347f6ca538ac53f2b5a9bf
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-06-02 15:09:27 -0300

    fix: lint arquivo css do header e arquivo OrdersClients

--- front-end/src/pages/OrdersClients.jsx
+++ front-end/src/pages/OrdersClients.jsx
@@ -1,22 +1,23 @@
+import React from 'react';
 import HeaderClients from '../components/HeaderClients';
 
-function OrdersClients () {
+function OrdersClients() {
   return (
+    <div>
+      <HeaderClients />
       <div>
-        <HeaderClients />
-        <div>
-          <h3>Pedido</h3>
-          <p>00001</p>
-        </div>
-        <div>
-          <h1>PENDENTE</h1>
-        </div>
-        <div>
-          <h2>Data</h2>
-          <h2>Valor</h2>
-        </div>      
-    </div> 
-  )
+        <h3>Pedido</h3>
+        <p>00001</p>
+      </div>
+      <div>
+        <h1>PENDENTE</h1>
+      </div>
+      <div>
+        <h2>Data</h2>
+        <h2>Valor</h2>
+      </div>
+    </div>
+  );
 }
 
 export default OrdersClients;
