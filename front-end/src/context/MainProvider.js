3e502ccb7b61df0954f409245a590f7010579361
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-06-02 17:00:57 -0300

    Adiciona card do pedidos do cliente

--- front-end/src/context/MainProvider.js
+++ front-end/src/context/MainProvider.js
@@ -1,17 +1,20 @@
-import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
+import React, { createContext, useState } from 'react';
 
 export const MainContext = createContext();
 
 function MainProvider({ children }) {
   const [teste, setTeste] = useState('teste123');
   const [clientName, setClientName] = useState('Nome do Cliente');
+  const [userId, setUserId] = useState('1');
 
   const shared = {
     teste,
     setTeste,
     clientName,
     setClientName,
+    userId,
+    setUserId,
   };
 
   return (
--- front-end/src/pages/OrdersClients.jsx
+++ front-end/src/pages/OrdersClients.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { genericApiResquest } from '../api';
 import HeaderClients from '../components/HeaderClients';
+import { MainContext } from '../context/MainProvider';
 
 function OrdersClients() {
+  const [orders, setOrders] = useState([]);
+  const { userId } = useContext(MainContext);
+
+  useEffect(() => {
+    genericApiResquest.get(`orders/clients/${userId}`).then((response) => {
+      setOrders([response.data]);
+    });
+  }, [userId]);
+
   return (
     <div>
       <HeaderClients />
-      <div>
-        <h3>Pedido</h3>
-        <p>00001</p>
-      </div>
-      <div>
-        <h1>PENDENTE</h1>
-      </div>
-      <div>
-        <h2>Data</h2>
-        <h2>Valor</h2>
-      </div>
+      {orders.map((order) => (
+        <section key={ order.id }>
+          <div>
+            <h3>Pedido</h3>
+            <p>{order.deliveryNumber}</p>
+          </div>
+          <div>
+            <h1>{order.status.toUpperCase()}</h1>
+          </div>
+          <div>
+            <h2>{order.saleDate}</h2>
+            <h2>{order.totalPrice}</h2>
+          </div>
+        </section>
+      ))}
     </div>
   );
 }
