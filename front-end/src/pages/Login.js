a0949dd75ab39f65ae9535bb3e5558a46b3d96dc
EddieRAM <edusacarneiro@gmail.com>
2022-06-03 12:33:40 -0300

    feat: login flux back and front

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,10 +1,102 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm';
-
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../api';
+import statusCodes from '../utils/statusCodes';
+import '../styles/Register.css';
 
 function Login() {
+  const [input, setInput] = useState({ email: '', password: '' });
+  const [inputValidation, setInputValidation] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const checkValidation = useCallback(() => {
+    const { email, password } = input;
+    const re = /\S+@\S+\.\S+/;
+    const MIN_PASSWORD_LENGTH = 6;
+
+    if (re.test(email) && password.length >= MIN_PASSWORD_LENGTH) {
+      setInputValidation(true);
+    } else {
+      setInputValidation(false);
+    }
+  }, [input]);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setInput((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const { email, password } = input;
+    const loginResult = await login(email, password);
+
+    if (loginResult.status === statusCodes.OK) {
+      navigate('/customer/products');
+    } else {
+      setMessage(loginResult.data.message);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    let control = true;
+    if (control) checkValidation();
+
+    return () => {
+      control = false;
+    };
+  }, [checkValidation]);
+
   return (
-    <LoginForm />
+    <main className="register-page">
+      <h1>Log In</h1>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="email-input">
+          <span>E-mail:</span>
+          <br />
+          <input
+            type="text"
+            id="email-input"
+            name="email"
+            onChange={ handleChange }
+            data-testid="common_login__input-email"
+          />
+        </label>
+        <label htmlFor="password-input">
+          <span>Senha:</span>
+          <br />
+          <input
+            type="password"
+            id="password-input"
+            name="password"
+            onChange={ handleChange }
+            data-testid="common_login__input-password"
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ !inputValidation }
+          data-testid="common_login__button-login"
+        >
+          {isLoading ? <div className="loader" /> : <span>Logar</span>}
+        </button>
+        <button
+          type="button"
+          onClick={ () => {
+            navigate('/register');
+          } }
+          data-testid="common_login__button-register"
+        >
+          <span>Registrar</span>
+        </button>
+      </form>
+      {message && (
+        <p data-testid="common_login__element-invalid-email">{message}</p>
+      )}
+    </main>
   );
 }
 
--- front-end/src/utils/statusCodes.js
+++ front-end/src/utils/statusCodes.js
@@ -2,6 +2,7 @@ const statusCodes = {
   CONFLICT: 409,
   CREATED: 201,
   NOT_FOUND: 404,
+  OK: 200,
 };
 
 export default statusCodes;
