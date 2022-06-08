b0efea6840d467926df700e8192ef7b831234adf
Danielen Cestari Nunes <danielennunes@Air-de-Danielen.lan>
2022-06-07 22:02:10 -0300

    resolve as treta

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import CustomerOrderDetail from './pages/CustomerOrderDetail';
 import Login from './pages/Login';
-
-import Login from './pages/Login';
 import Register from './pages/Register';
-import Products from './pages/Products';
-import OrderClients from './pages/OrdersClients';
 import Admin from './pages/Admin';
 import NotFound from './pages/NotFound';
 import CustomerOrder from './pages/CustomerOrder';
 import Products from './pages/Products';
-import Register from './pages/Register';
 import CheckoutClient from './pages/CheckoutClient';
 import './styles/App.css';
 
